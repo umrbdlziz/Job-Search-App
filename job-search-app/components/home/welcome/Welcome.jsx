b976eda8fe23d1066bfaa4a2d7d4a323d390b011
umxziz <umxziz@student.usm.my>
2023-11-19 07:32:06 +0800

    Create new font
    build a new created font

--- job-search-app/components/home/welcome/Welcome.jsx
+++ job-search-app/components/home/welcome/Welcome.jsx
@@ -21,7 +21,17 @@ const Welcome = () => {
         <Text style={styles.userName}>Hello Abu</Text>
         <Text style={styles.welcomeMessage}>Find your perfect job</Text>
       </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput 
+            style={styles.searchInput}
+          />
+        </View>
+      </View>
+
     </View>
+
   )
 }
 
