d92f2212805cec9c44d38e8d80998bba84e084cb
umxziz <umxziz@student.usm.my>
2023-11-21 07:04:38 +0800

    Import fetch component
    import useFetch into Popularjobs

--- job-search-app/hook/useFetch.js
+++ job-search-app/hook/useFetch.js
@@ -1,8 +1,5 @@
 import { useState, useEffect } from "react";
 import axios from 'axios';
-import { RAPID_API_KEY } from '@env';
-
-const rapidApiKey = RAPID_API_KEY;
 
 const useFetch = (endpoint, query) => {
     const [data, setData] = useState([]);
@@ -13,11 +10,38 @@ const useFetch = (endpoint, query) => {
         method: 'GET',
         url: `https://jsearch.p.rapidapi.com/${endpoint}`,
         headers: {
-          'X-RapidAPI-Key': rapidApiKey,
+          'X-RapidAPI-Key': '2abdd60117msh8cccf89daf76a7fp1840fbjsn8d6cd60aab8c',
           'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
         },
-        params: {
-            ...query
-        }
+        params: { ...query }
       };
-}
+
+      const fetchData = async () => {
+        setIsLoading(true);
+
+        try{
+            const response = await axios.request
+            (options);
+
+            setData(response.data.data);
+            setIsLoading(false);
+        } catch (error) {
+            setError(error);
+            alert(`There is an error`)
+        } finally {
+            setIsLoading(false);
+        }
+      }
+
+      useEffect(() => {
+        fetchData();
+      }, []);
+
+      const refetch = () => {
+        setIsLoading(true);
+        fetchData
+      }
+
+      return { data, isLoading, error, refetch };
+    }
+    export default useFetch;
