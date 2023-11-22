bc3f75715058b19110cb3c422f6162adf9000e54
umxziz <umxziz@student.usm.my>
2023-11-23 06:55:56 +0800

    add company and jobtabs component
    add component and their props

--- job-search-app/app/job-details/[id].js
+++ job-search-app/app/job-details/[id].js
@@ -48,7 +48,26 @@ const JobDetails = () => {
             <ScrollView showsVerticalScrollIndicator={false} refreshControl={
                 <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
             }>
+                {isLoading ? (
+                    <ActivityIndicator size="large" color={COLORS.primary} />
+                ) : error ? (
+                    <Text>Something went wrong</Text>
+                ) : data.length === 0 ? (
+                    <Text>No data</Text>
+                ) : (
+                    <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+                        <Company
+                            companyLogo={data[0].employer_logo}
+                            jobTitle={data[0].job_title}
+                            companyName={data[0].employer_name}
+                            location={data[0].job_country}
+                        />
 
+                        <JobTabs
+
+                        />
+                    </View>
+                )}
             </ScrollView>
         </>
     </SafeAreaView>
